d3c1a2d5e6716fc3ccaf7d5c0cf2d72b18185884
Justin Loh <justinlohkingwei@gmail.com>
2023-09-26 11:18:42 -0400

    indexing of EXP50 done

--- src/sections/BetterExpModule.tsx
+++ src/sections/BetterExpModule.tsx
@@ -1,19 +1,44 @@
 import styled from "styled-components";
 import Section from "../components/Section";
-import { Paper, TextField } from "@mui/material";
+import { Button, Paper, TextField } from "@mui/material";
 import Exp50 from "./ExpModules/Exp50";
+import { useContext, useState } from "react";
+import { ExpansionKey, defaultExpansionKey } from "../data/Templates";
+import { globalContextTypes, GlobalContext } from "../App";
 
 const Container = styled.div`
   display: flex;
   width: 100%;
   min-height: 5rem;
-  justify-content: space-evenly;
+`;
+
+const InputContainer = styled.div`
+  width: 45%;
+  padding: auto;
 `;
 
 const InputSection = styled(Paper)`
   background-color: #8080802f;
-  width: 25%;
   height: 23rem;
+  padding-top: 1rem;
+`;
+
+const SubInputSection = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding: 2rem 0 2rem 0;
+`;
+
+const StyledButton = styled(Button)`
+  display: flex;
+  background-color: green;
+  color: white;
+  width: 10rem;
+  height: 4rem;
+  &:hover {
+    background-color: #00800068;
+    color: black;
+  }
 `;
 
 const LineSection = styled.div`
@@ -22,17 +47,152 @@ const LineSection = styled.div`
   gap: 2rem;
 `;
 
+type expkeyType = {
+  number: number;
+  name: string;
+};
+
+const expkeyTypes: expkeyType[] = [
+  { number: 0, name: "NA" },
+  { number: 1, name: "Conference" },
+  { number: 2, name: "Forward" },
+  { number: 3, name: "Transfer" },
+  { number: 4, name: "Hold" },
+  { number: 5, name: "DND" },
+  { number: 7, name: "Call Return" },
+  { number: 8, name: "SMS" },
+  { number: 9, name: "Directed Pickup" },
+  { number: 10, name: "Call Park" },
+  { number: 11, name: "DTMF" },
+  { number: 12, name: "Voicemail" },
+  { number: 13, name: "SpeedDial" },
+  { number: 14, name: "Intercom" },
+  { number: 15, name: "Line" },
+  { number: 16, name: "BLF" },
+  { number: 17, name: "URL" },
+  { number: 18, name: "Group Listening" },
+  { number: 20, name: "Private Hold" },
+  { number: 22, name: "XML Group" },
+  { number: 23, name: "Group Pickup" },
+  { number: 24, name: "Multicast Paging" },
+  { number: 25, name: "Record" },
+  { number: 27, name: "XML Browser" },
+  { number: 34, name: "Hot Desking" },
+  { number: 35, name: "URL Record" },
+  { number: 38, name: "LDAP" },
+  { number: 39, name: "BLF List" },
+  { number: 40, name: "Prefix" },
+  { number: 41, name: "Zero Touch" },
+  { number: 42, name: "ACD" },
+  { number: 45, name: "Local Group" },
+  { number: 46, name: "Network Group" },
+  { number: 49, name: "Custom Button" },
+  { number: 50, name: "Keypad Lock" },
+  { number: 55, name: "Meet-Me Conference" },
+  { number: 58, name: "Retrieve Park" },
+  { number: 59, name: "ACD Grace" },
+  { number: 60, name: "Emergency" },
+  { number: 61, name: "Directory" },
+];
+
 function BetterExpModule() {
+  const { currentTemplate, setcurrentTemplate }: globalContextTypes =
+    useContext(GlobalContext);
+  const [currentExpKey, setcurrentExpKey] =
+    useState<ExpansionKey>(defaultExpansionKey);
+  const [flipped, setFlipped] = useState(false);
+
   return (
     <Section title="Expansion Module Configuration">
       <Container>
-        <InputSection>
-        <LineSection> <TextField/></LineSection>
-        <LineSection> <TextField/></LineSection>
-        <LineSection> <TextField/></LineSection>
-        <LineSection> <TextField/></LineSection>
-        </InputSection>
-       <Exp50></Exp50>
+        <InputContainer>
+          <InputSection>
+            <LineSection>
+              {" "}
+              <TextField
+                label="Module Number"
+                variant="outlined"
+                defaultValue={currentExpKey.module_number || ""}
+                sx={{
+                  width: "8rem",
+                }}
+                disabled
+              />
+            </LineSection>
+            <LineSection>
+              {" "}
+              <TextField
+                label="Line Number"
+                variant="outlined"
+                defaultValue={currentExpKey.line_number || ""}
+                sx={{
+                  width: "8rem",
+                }}
+                onChange={(e) => {
+                  const newValue = parseInt(e.target.value);
+                  if (currentTemplate.expansionkeys !== undefined) {
+                    const expkeysCopy = [...currentTemplate.expansionkeys];
+                    expkeysCopy[currentExpKey.line_number].line_number =
+                      newValue;
+                    setcurrentTemplate({
+                      ...currentTemplate,
+                      expansionkeys: expkeysCopy,
+                    });
+                  }
+                }}
+              />
+            </LineSection>
+            <LineSection>
+              {" "}
+              <TextField
+                label="Label"
+                variant="outlined"
+                defaultValue={currentExpKey.label || ""}
+                onChange={(e) => {
+                  const newValue = e.target.value;
+                  if (currentTemplate.expansionkeys !== undefined) {
+                    const expkeysCopy = [...currentTemplate.expansionkeys];
+                    expkeysCopy[currentExpKey.line_number].label = newValue;
+                    setcurrentTemplate({
+                      ...currentTemplate,
+                      expansionkeys: expkeysCopy,
+                    });
+                  }
+                }}
+              />
+            </LineSection>
+            <LineSection>
+              {" "}
+              <TextField
+                label="Value"
+                variant="outlined"
+                defaultValue={currentExpKey.value || ""}
+                onChange={(e) => {
+                  const newValue = e.target.value;
+                  if (currentTemplate.expansionkeys !== undefined) {
+                    const expkeysCopy = [...currentTemplate.expansionkeys];
+                    expkeysCopy[currentExpKey.line_number].value = newValue;
+                    setcurrentTemplate({
+                      ...currentTemplate,
+                      expansionkeys: expkeysCopy,
+                    });
+                  }
+                }}
+              />
+            </LineSection>
+          </InputSection>
+          <SubInputSection>
+            <StyledButton
+              onClick={() => {
+                const b = !flipped;
+                setFlipped(b);
+              }}
+            >
+              Flip
+            </StyledButton>
+          </SubInputSection>
+        </InputContainer>
+        <Exp50 flipped={flipped} />
       </Container>
     </Section>
   );
--- src/sections/ExpModules/Exp50.tsx
+++ src/sections/ExpModules/Exp50.tsx
@@ -1,10 +1,12 @@
 import { Paper } from "@mui/material";
 import styled from "styled-components";
+import { ExpansionKey, defaultExpansionKey } from "../../data/Templates";
 
 const Exp50Root = styled(Paper)`
   background-color: #383737;
-  width: 50%;
+  width: 30%;
   border-radius: 10px;
+  margin: auto;
 `;
 
 const Header = styled.div`
@@ -39,28 +41,82 @@ const ScreenSection = styled.div`
   height: 3rem;
 `;
 
+const ScreenNames = styled.div`
+  display: flex;
+  min-width: 80%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem 0 1rem;
+`;
 const BottomSection = styled.div`
   display: flex;
-  background-color: #383737;
-  justify-content: space-evenly;
+  background-color: #2a2a36;
+  justify-content: center;
   align-items: center;
   min-height: 10rem;
+  margin-top: 3rem;
 `;
 
-function Exp50() {
-  const noOfLines = 10;
+const PageButton = styled.div`
+  display: flex;
+  height: 3rem;
+  width: 3rem;
+  background-color: silver;
+  justify-content: center;
+  margin: 1rem;
+  align-items: center;
+  &:hover {
+    background-color: #c0c0c07b;
+  }
+`;
 
+const testExpKeyList: ExpansionKey[] = [
+  { ...defaultExpansionKey, module_number: 1, line_number: 1 },
+  { ...defaultExpansionKey, module_number: 1, line_number: 2 },
+  { ...defaultExpansionKey, module_number: 2, line_number: 1 },
+  { ...defaultExpansionKey, module_number: 2, line_number: 2 },
+  { ...defaultExpansionKey, module_number: 2, line_number: 3 },
+  { ...defaultExpansionKey, module_number: 3, line_number: 1 },
+  { ...defaultExpansionKey, module_number: 3, line_number: 2 },
+  { ...defaultExpansionKey, module_number: 3, line_number: 3 },
+  { ...defaultExpansionKey, module_number: 3, line_number: 4 },
+];
+
+type Exp50Props = {
+  flipped: boolean;
+};
+
+function Exp50({ flipped }: Exp50Props) {
+  const noOfLines = 10;
   return (
     <Exp50Root>
       <Header>Yealink</Header>
-      {Array.from({ length: noOfLines }).map((_, index) => (
-        <ExpLineSection>
-          <SideButton />
-          <ScreenSection />
-          <SideButton />
-        </ExpLineSection>
-      ))}
-      <BottomSection />
+      {Array.from({ length: noOfLines }).map((_, index) => {
+        const pairIndex =  flipped? index + index :index;
+        const i = flipped? pairIndex+1 : pairIndex+1;
+        const j = flipped? pairIndex+2: pairIndex+11;
+        return (
+          <ExpLineSection>
+            <SideButton />
+            <ScreenSection>
+              <ScreenNames>
+                {/* <p>{index}</p>
+                <p>{index + 10}</p> */}
+                {/* <p>{index+1}</p>
+                <p>{index + 2}</p> */}
+                  <p>{i}</p>
+                <p>{j}</p>
+              </ScreenNames>
+            </ScreenSection>
+            <SideButton />
+          </ExpLineSection>
+        );
+      })}
+      <BottomSection>
+        <PageButton>1</PageButton>
+        <PageButton>2</PageButton>
+        <PageButton>3</PageButton>
+      </BottomSection>
     </Exp50Root>
   );
 }
